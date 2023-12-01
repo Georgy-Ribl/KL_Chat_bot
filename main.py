1336345e433c99cc0361cae3f25947436da413e4
Georgy-Ribl <geokorneev2006@gmail.com>
2023-12-01 22:58:05 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import telebot
+from config import TOKEN
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, message.text)
+
+bot.infinity_polling()
